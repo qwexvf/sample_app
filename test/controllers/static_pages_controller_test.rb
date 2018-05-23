83d4e4f440342073f7da69b79bfe9a1792c9b401
qwexvf <qw3xvf@gmail.com>
2018-05-23 18:57:01 +0900

    add exercise

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+	# test "should get root" do
+	#	 get root_path
+	#	 assert_response :success
+	# end
+
   test "should get home" do
     get root_path
     assert_response :success
